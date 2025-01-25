30d2b218214214c178f94b9876d59749a88f5004
BalrajeGaikwad <balrajegaikwad55@gmail.com>
2025-01-25 23:33:19 +0530

    program file

---
+++ convert_1.py
@@ -0,0 +1,15 @@
+"""(b) Write a program to convert the following string
+
+'Visit ykanetkar.com for online courses in programming'
+
+into
+
+'Visit Ykanetkar.com For Online Courses In Programming'
+
+
+"""
+
+string='Visit ykanetkar.com for online courses in programming'
+
+new=string.title()
+print(new)
